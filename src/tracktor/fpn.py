8a8cbf5914391fde5735780623ce9a1c27bec0cd
Carolin <carolin.hecking@tum.de>
2019-11-17 22:15:12 +0100

    remove Variable

--- src/tracktor/fpn.py
+++ src/tracktor/fpn.py
@@ -63,7 +63,7 @@ class FPN(FPNResNet):
     def load_image(self, image, im_info):
         permuted_image = image.permute(0, 3, 1, 2)
 
-        self.im_data = Variable(permuted_image)
+        self.im_data = permuted_image
         if torch.cuda.is_available():
             print("Cuda is available: {}".format(torch.cuda.is_available()))
             self.im_data.cuda()
