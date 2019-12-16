be285f35f142d4f3fb98eacb528e7982d60f42df
Carolin <carolin.hecking@tum.de>
2019-12-16 20:41:47 +0100

    Add config yaml files

---
+++ experiments/scripts/run_hp_search.py
@@ -0,0 +1,8 @@
+import os
+
+os.system("python experiments/scripts/hp_search_finetune_tracktor.py with cfg1")
+os.system("python experiments/scripts/hp_search_finetune_tracktor.py with cfg2")
+os.system("python experiments/scripts/hp_search_finetune_tracktor.py with cfg3")
+os.system("python experiments/scripts/hp_search_finetune_tracktor.py with cfg4")
+os.system("python experiments/scripts/hp_search_finetune_tracktor.py with cfg5")
+os.system("python experiments/scripts/hp_search_finetune_tracktor.py with cfg6")
