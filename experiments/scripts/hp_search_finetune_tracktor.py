06536765e18bceb54c67d6c672ad826bfd60a085
Carolin <carolin.hecking@tum.de>
2019-12-18 14:15:21 +0100

    Force finetuning true in configs

--- experiments/scripts/hp_search_finetune_tracktor.py
+++ experiments/scripts/hp_search_finetune_tracktor.py
@@ -36,6 +36,7 @@ ex.add_named_config('cfg7', 'experiments/cfgs/hp_search/cfg7.yaml')
 ex.add_named_config('cfg8', 'experiments/cfgs/hp_search/cfg8.yaml')
 ex.add_named_config('cfg9', 'experiments/cfgs/hp_search/cfg9.yaml')
 ex.add_named_config('cfg10', 'experiments/cfgs/hp_search/cfg10.yaml')
+ex.add_named_config('cfg11', 'experiments/cfgs/hp_search/cfg11.yaml')
 
 ########### DEFAULT################
 
