6636a6998bd8d58424922f2385f515e66b50e281
Adrian Ziegler <adrian.ziegler@student.hpi.de>
2019-12-23 17:13:37 +0100

    set validation over time default to False

--- src/tracktor/vis_results.py
+++ src/tracktor/vis_results.py
@@ -2,15 +2,17 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-file_names = ["1", "2", "3", "4", "5", "6"]
+file_names = ["all_results_baseline", "results_1_0.15_128_1e-4_4", "results_2_0.1_128_1e-4_4",
+              "results_3_0.12_128_1e-4_4", "results_4_0.08_128_1e-4_4", "results_5_0.1_128_5e-5_4",
+              "results_6_0.1_16_1e-4_4"]
 metrics_to_plot = ["idf1", "mota", "num_misses", "num_false_positives", "num_switches"]
-sequences = ["OVERALL"]
+sequences = ["OVERALL", "MOT17-13-FRCNN"]
 
 array = np.array([])
 res = pd.DataFrame()
 
 for f in file_names:
-    df = pd.read_pickle("output/finetuning_results/all_results_{}.pkl".format(f))
+    df = pd.read_pickle("../../output/finetuning_results/{}.pkl".format(f))
     #for sequence in sequences:
     res = res.append(df.loc[sequences, metrics_to_plot])
 
