58211c24deb7b713960d3b6def35e9d8aa83d546
Adrian Ziegler <adrian.ziegler@student.hpi.de>
2019-12-23 21:38:24 +0100

    fix bug

--- src/tracktor/tracker.py
+++ src/tracktor/tracker.py
@@ -395,12 +395,12 @@ class Tracker:
             if len(self.tracks):
                 # create nms input
                 if frame==2:
-                    print(box_iou(*[t.pos() for t in self.tracks if t.id in [2, 0]]))
+                    print(box_iou(*[t.pos for t in self.tracks if t.id in [2, 0]]))
                 # nms here if tracks overlap
                 keep = nms(self.get_pos(), person_scores, self.regression_nms_thresh)
                 if frame == 526 or frame == 527 or frame == 528:
                     print(keep)
-                    print(box_iou(*[t.pos() for t in self.tracks if t.id in [15, 25]]))
+                    print(box_iou(*[t.pos for t in self.tracks if t.id in [15, 25]]))
                 self.tracks_to_inactive([self.tracks[i] for i in list(range(len(self.tracks))) if i not in keep])
 
                 if frame == 526 or frame == 527 or frame == 528:
