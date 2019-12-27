608ce68314ad51f66e3b0ca1ddeafdf712ca7917
Adrian Ziegler <adrian.ziegler@student.hpi.de>
2019-12-27 14:31:41 +0100

    remove unused script calls

--- experiments/scripts/run_hp_search.py
+++ experiments/scripts/run_hp_search.py
@@ -1,12 +1,3 @@
 import os
 
 os.system("python experiments/scripts/hp_search_finetune_tracktor.py with cfg1")
-os.system("python experiments/scripts/hp_search_finetune_tracktor.py with cfg2")
-os.system("python experiments/scripts/hp_search_finetune_tracktor.py with cfg3")
-os.system("python experiments/scripts/hp_search_finetune_tracktor.py with cfg4")
-os.system("python experiments/scripts/hp_search_finetune_tracktor.py with cfg5")
-os.system("python experiments/scripts/hp_search_finetune_tracktor.py with cfg6")
-
-#os.system("python experiments/scripts/hp_search_finetune_tracktor.py with cfg7")
-#os.system("python experiments/scripts/hp_search_finetune_tracktor.py with cfg8")
-#os.system("python experiments/scripts/hp_search_finetune_tracktor.py with cfg9")
