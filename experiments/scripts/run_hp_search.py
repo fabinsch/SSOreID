e0a64e15e54c9ea01cc3b22fff62c3a15ae213eb
Adrian Ziegler <adrian.ziegler@student.hpi.de>
2019-12-28 12:45:18 +0100

    make experiments run remotely triggered by IDE <

--- experiments/scripts/run_hp_search.py
+++ experiments/scripts/run_hp_search.py
@@ -1,3 +1,3 @@
 import os
 
-os.system("python experiments/scripts/hp_search_finetune_tracktor.py with cfg1")
+os.system("/opt/anaconda3/bin/python -u experiments/scripts/hp_search_finetune_tracktor.py with cfg1")
