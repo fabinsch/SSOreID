82a62ea255a2fce51e27a26ab2514f02017d1561
Adrian Ziegler <adrian.ziegler@student.hpi.de>
2020-01-13 14:14:40 +0100

    add corresponding run file

--- experiments/scripts/run_hp_search.py
+++ experiments/scripts/run_hp_search.py
@@ -1,5 +1,3 @@
 import os
 
-os.system("/opt/anaconda3/bin/python -u experiments/scripts/hp_search_finetune_tracktor.py with cfg_caro_ws1")
-os.system("/opt/anaconda3/bin/python -u experiments/scripts/hp_search_finetune_tracktor.py with cfg_caro_ws2")
-os.system("/opt/anaconda3/bin/python -u experiments/scripts/hp_search_finetune_tracktor.py with cfg_caro_ws3")
+os.system("/opt/anaconda3/bin/python -u experiments/scripts/hp_search_finetune_tracktor.py with cfg1")
