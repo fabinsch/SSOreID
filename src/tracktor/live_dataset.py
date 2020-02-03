0058dfd3bc8bc22e786a9ea8b63e6f8226938283
Adrian Ziegler <adrian.ziegler@student.hpi.de>
2020-02-03 15:47:54 +0100

    remove samples from data set when reaching a threshold

--- src/tracktor/live_dataset.py
+++ src/tracktor/live_dataset.py
@@ -14,7 +14,7 @@ class IndividualDataset(torch.utils.data.Dataset):
         self.scores = torch.tensor([]).to(device)
         self.samples_per_frame = None
         self.number_of_positive_examples = None
-        self.keep_frames = 50
+        self.keep_frames = 40
         self.num_frames = 0
 
     def append_samples(self, training_set_dict):
@@ -22,25 +22,31 @@ class IndividualDataset(torch.utils.data.Dataset):
         self.features = torch.cat((self.features, training_set_dict['features']))
         self.boxes = torch.cat((self.boxes, training_set_dict['boxes']))
         self.scores = torch.cat((self.scores, training_set_dict['scores']))
-        if self.num_frames == self.keep_frames:
+        if self.num_frames > self.keep_frames:
             self.remove_samples()
 
+    BATCH_SIZE = 64
     def remove_samples(self):
-        #TODO remove first batch size big samples
-        return
+        self.boxes = self.boxes[self.BATCH_SIZE:, :]
+        self.scores = self.scores[self.BATCH_SIZE:]
+        self.features = self.features[self.BATCH_SIZE:, :, :, :]
 
     # Filter out all duplicates and add frame number tensor for each data point
-    NUMBER_OF_POSITIVE_EXAMPLE_DUPLICATES = 15
+    NUMBER_OF_POSITIVE_EXAMPLE_DUPLICATES = 31
     def post_process(self):
         self.samples_per_frame = defaultdict(list)
         unique_indices = []
         number_of_duplicates = 0
         frame_number = 0
         current_box = self.boxes[0, :]
+        saw_positive = False
         for i, box in enumerate(torch.cat((self.boxes[1:, :], torch.Tensor([[0,0,0,0]]).to(device)))):
             if not torch.equal(box, current_box):
-                if number_of_duplicates == self.NUMBER_OF_POSITIVE_EXAMPLE_DUPLICATES:
+                if number_of_duplicates == self.NUMBER_OF_POSITIVE_EXAMPLE_DUPLICATES and not saw_positive:
                     frame_number += 1
+                    saw_positive = True
+                else:
+                    saw_positive = False
                 unique_indices.append(i)
                 self.samples_per_frame[frame_number].append(i)
                 current_box = box
@@ -66,8 +72,15 @@ class IndividualDataset(torch.utils.data.Dataset):
             self.samples_per_frame[frame_number] = box_idx_in_frame_sorted
 
     def get_training_set(self):
-        training_set, _ = self.val_test_split(num_frames_train=self.number_of_positive_examples, num_frames_val=0, train_val_frame_gap=0)
-        return training_set
+        num_train = 40 if self.number_of_positive_examples > 40 else self.number_of_positive_examples
+        training_set, _ = self.val_test_split(num_frames_train=num_train, num_frames_val=0, train_val_frame_gap=0,
+                                              downsampling=False)
+        #if self.number_of_positive_examples >= 5:
+        #    training_set, val_set = self.val_test_split(num_frames_train=num_train-1, num_frames_val=1,
+        #                                                train_val_frame_gap=0)
+        #    return training_set, val_set
+
+        return training_set, training_set
 
     def val_test_split(self, num_frames_train=20, num_frames_val=10, train_val_frame_gap=0, downsampling=True,
                        shuffle=True):
@@ -75,8 +88,10 @@ class IndividualDataset(torch.utils.data.Dataset):
             "There are not enough frames in the data set"
         pos_idx_train = []
         neg_idx_train = []
-        if num_frames_val == 0 and train_val_frame_gap == 0:
-            for i in range(self.number_of_positive_examples - num_frames_train, self.number_of_positive_examples):
+        pos_idx_val = []
+        neg_idx_val = []
+        if train_val_frame_gap == 0:
+            for i in range(self.number_of_positive_examples - num_frames_train - num_frames_val, self.number_of_positive_examples - num_frames_val):
                 pos_idx_for_frame = self.samples_per_frame[i + 1][0]
                 pos_idx_train.append(pos_idx_for_frame)
                 if downsampling:
@@ -87,8 +102,23 @@ class IndividualDataset(torch.utils.data.Dataset):
                     repeated_pos_frame = [pos_idx_for_frame] * (len(neg_idx_for_frame) - 1)
                     pos_idx_train.extend(repeated_pos_frame)
                     neg_idx_train.extend(neg_idx_for_frame)
+
+            for i in range(self.number_of_positive_examples - num_frames_val, self.number_of_positive_examples):
+                pos_idx_for_frame = self.samples_per_frame[i + 1][0]
+                print(self.boxes[pos_idx_for_frame, :])
+                pos_idx_val.append(pos_idx_for_frame)
+                if downsampling:
+                    # Choose the box as negative example that has highest iou with positive example box
+                    neg_idx_val.append(self.samples_per_frame[i + 1][1])
+                else:
+                    neg_idx_for_frame = self.samples_per_frame[i + 1][1:]
+                    repeated_pos_frame = [pos_idx_for_frame] * (len(neg_idx_for_frame) - 1)
+                    pos_idx_val.extend(repeated_pos_frame)
+                    neg_idx_val.extend(neg_idx_for_frame)
+
             train_idx = torch.cat((torch.LongTensor(pos_idx_train), torch.LongTensor(neg_idx_train)))
-            return  [Subset(self, train_idx), []]
+            val_idx = torch.cat((torch.LongTensor(pos_idx_val), torch.LongTensor(neg_idx_val)))
+            return  [Subset(self, train_idx), Subset(self, val_idx)]
 
         for frame_number in range(num_frames_train):
             pos_idx_for_frame = self.samples_per_frame[frame_number + 1][0]
@@ -102,8 +132,6 @@ class IndividualDataset(torch.utils.data.Dataset):
                 pos_idx_train.extend(repeated_pos_frame)
                 neg_idx_train.extend(neg_idx_for_frame)
 
-        pos_idx_val = []
-        neg_idx_val = []
         for frame_number in range(num_frames_train + train_val_frame_gap, num_frames_train + train_val_frame_gap + num_frames_val):
             pos_idx_for_frame = self.samples_per_frame[frame_number+1][0]
             pos_idx_val.append(pos_idx_for_frame)
